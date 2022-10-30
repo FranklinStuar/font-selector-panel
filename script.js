5cd222c3d7d9a05bdf470ece65c8ca6ad5e991eb
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2022-10-29 23:22:44 -0500

    json para tipografia manual

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-alert("hello world")
+const listFonts = [
+  {
+    name: "Roboto",
+    family: ['Roboto', "sans-serif"],
+    weight: [100,300,400,500,700,800,900,"normal","bold"],
+    style: ["normal","italic"],
+    default: {style:"normal",weight:"normal"}
+  },
+  {
+    name: "Poppins",
+    family: ['Poppins', "sans-serif"],
+    weight: [100,300,400,500,700,800,900,"normal","bold"],
+    style: ["normal","italic"],
+    default: {style:"normal",weight:"normal"}
+  },
+  {
+    name: "Poor Story",
+    family: ['Poor Story', "cursive"],
+    weight: [400,"normal"],
+    style: ["normal"],
+    default: {style:"normal",weight:"normal"}
+  },
+  {
+    name: "Island Moments",
+    family: ['Island Moments', "cursive"],
+    weight: [400,"normal"],
+    style: ["normal"],
+    default: {style:"normal",weight:"normal"}
+  },
+]
